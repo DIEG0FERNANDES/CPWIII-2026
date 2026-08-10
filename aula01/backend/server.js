9fb272a4798d02f5c3837d2f6d2999b97bd17cbc
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2026-08-10 19:19:08 -0400

    Initial commit

---
+++ aula01/backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
