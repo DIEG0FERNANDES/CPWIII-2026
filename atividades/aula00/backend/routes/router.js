096b0e66fe61128676a8f6aef5b70a7cc4652046
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2026-08-10 20:55:03 -0400

    Primeiro Commit
    Erro de servidor não funcionando por conta de sintaxes escritos de forma incorreta corrigido

--- atividades/aula00/backend/routes/router.js
+++ atividades/aula00/backend/routes/router.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
-import app from "../app";
 
-const router = Router;
+const router = Router();
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Seja bem vindo! Este é o servidor backend funcionando");
 });
-// app.get("/aluno", (req, res) => {
-//   res.send("rota ok");
-// });
-// app.get("/aluno/:a/:b", (req, res) => {
-//   const a = Number(req.params.a);
-//   const b = Number(req.params.b);
-//   const resultado = a + b;
-//   res.send(`O resultado é, ${resultado}`);
-// });
+
+router.get("/aluno", (req, res) => {
+  res.send("rota ok");
+});
+
+router.get("/aluno/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const resultado = a + b;
+  res.send(`O resultado é, ${resultado}`);
+});
 
 export default router;
